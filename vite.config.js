4cf3ade85ac4a736b259d5a79fd840d5a30a36c7
Ashutosh Mohanty <41833078+usernameAshu@users.noreply.github.com>
2025-01-08 21:18:36 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import {defineConfig} from "vite"
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [
-		
-	]
-})
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'index.html',
+                portfolio: 'src/pages/portfolio-item.html',
+            },
+        },
+    },
+});
