98ac7c377c69ebb9cab1e820d1fb5a7b15caf49c
Ashutosh Mohanty <41833078+usernameAshu@users.noreply.github.com>
2025-01-08 21:24:12 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
             input: {
                 main: 'index.html',
                 portfolio: 'src/pages/portfolio-item.html',
+				js: 'src/js/index.js'
             },
         },
     },
